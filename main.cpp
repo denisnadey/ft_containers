436dfc85426774e2f87c3071e170e3e2d00ae267
Matthos Denys <mdenys@un-b2.kzn.21-school.ru>
2022-01-23 21:46:03 +0300

    all files

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "containers/Vector.hpp"
+
+using namespace ft;
+
+
+int main() {
+
+
+    return 0;
+}
+
+
+/*
+ *
+ * Task one
+ * почитать все коменты []
+ *
+ * Task two
+ * explicit почитать []
+ *
+ *
+ */
