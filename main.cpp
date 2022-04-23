8fecd32bde4040990a92f256037c1fda85719c9a
Matthos Denys <mdenys@un-i1.kzn.21-school.ru>
2022-04-23 19:52:22 +0300

    added stack container

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "Vector.hpp"
+#include "vector.hpp"
 
 using namespace ft;
 
@@ -17,15 +17,15 @@ using namespace ft;
 
 int main() {
 
-    ft::vector<std::string> first;
-    ft::vector<std::string> second (5,"da");
-    ft::vector<std::string> third (second.begin(), second.end());
-//    ft::vector<std::string> fourth (third);
+    ft::vector<std::string> test ;
+    test.push_back("da");
+    ft::vector<std::string>::iterator iterator = test.end();
+    test.insert(iterator, "kek");
 
-//    ft::vector<std::string> first_value;
-//    first_value.push_back("da");
-
-
+    for (ft::vector<std::string>::iterator it = test.begin();  it != test.end(); it++ )
+    {
+        std::cout << *it << std::endl;
+    }
 
     return 0;
 }
